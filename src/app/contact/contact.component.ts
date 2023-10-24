f1245528e032135e5b23a8da8f3df10510ac26fa
MustafaSoycan <mustafa.soycan@outlook.com>
2023-10-25 00:30:27 +0200

    Changed email size on small devices

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,39 +6,52 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-@ViewChild('myForm') myForm!: ElementRef;
-@ViewChild('nameField') nameField!: ElementRef;
-@ViewChild('emailField') emailField!: ElementRef;
-@ViewChild('messageField') messageField!: ElementRef;
-@ViewChild('sendButton') sendButon!: ElementRef;
+  @ViewChild('myForm') myForm!: ElementRef;
+  @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
+  @ViewChild('messageField') messageField!: ElementRef;
+  @ViewChild('sendButton') sendButon!: ElementRef;
+
+  disableFormFields() {
+    this.nameField.nativeElement.disabled = true;
+    this.emailField.nativeElement.disabled = true;
+    this.messageField.nativeElement.disabled = true;
+    this.sendButon.nativeElement.disabled = true;
+  }
+
+  enableFormFields() {
+    this.nameField.nativeElement.disabled = false;
+    this.emailField.nativeElement.disabled = false;
+    this.messageField.nativeElement.disabled = false;
+    this.sendButon.nativeElement.disabled = false;
+  }
 
   async sendMail() {
-      console.log('sending mail', this.myForm);
-      let nameField = this.nameField.nativeElement;
-      let emailField = this.emailField.nativeElement;
-      let messageField = this.messageField.nativeElement;
-      let sendButton = this.sendButon.nativeElement;
+    console.log('sending mail', this.myForm);
+
+    this.disableFormFields();
+
+    let fd = new FormData();
+    fd.append('name', this.nameField.nativeElement.value);
+    fd.append('email', this.emailField.nativeElement.value);
+    fd.append('message', this.messageField.nativeElement.value);
+
+    try {
+      await fetch('https://mustafa-soycan.com/send_mail/send_mail.php', {
+        method: 'post',
+        body: fd
+      });
+
+      // Nachricht erfolgreich gesendet - Setze die Formularfelder zurück
+      this.myForm.nativeElement.reset();
+    } catch (error) {
+      // Bei einem Fehler kannst du hier eine Fehlerbehandlung durchführen
+      console.error('Fehler beim Senden der Nachricht:', error);
+    } finally {
+
       
-      nameField.disabled = true;
-      emailField.disabled = true;
-      messageField.disabled = true;
-      sendButton.disabled = true;
-    // LADE ANIMATION
-      let fd = new FormData();
-      fd.append('name', nameField.value);
-      fd.append('email', emailField.value);
-      fd.append('message', messageField.value);
-
-    await fetch('https://mustafa-soycan.com/send_mail/send_mail.php',
-    {
-      method: 'post',
-      body: fd
+      // Aktiviere die Eingabefelder und den Senden-Button nach dem Senden oder bei Fehlern
+      this.enableFormFields();
     }
-    )
-
-      nameField.disabled = false;
-      emailField.disabled = false;
-      messageField.disabled = false;
-      sendButton.disabled = false;
   }
 }
