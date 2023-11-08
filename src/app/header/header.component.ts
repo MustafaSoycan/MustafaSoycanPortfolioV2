71d52d9ae3567ef8c472d1d6f3c4ebc6e9fad07e
MustafaSoycan <mustafa.soycan@outlook.com>
2023-11-08 21:42:33 +0100

    Added jsdoc to header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,23 +2,38 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedService } from '../shared.service';
 
+/**
+ * Die HeaderComponent ist verantwortlich für das Header-Element der Anwendung.
+ */
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent {
+
+  /**
+   * Gibt an, ob das Hamburger-Menü aktiv ist oder nicht.
+   */
   isHamburgerActive: boolean = false;
 
+  /**
+  * Erstellt eine neue Instanz der HeaderComponent.
+  * @param router - Der Angular Router für die Navigation.
+  * @param sharedService - Der gemeinsame Service für den Zugriff auf den Status des Hamburger-Menüs.
+  */
   constructor(private router: Router, private sharedService: SharedService) {
     this.sharedService.isHamburgerActive$.subscribe((isActive) => {
       this.isHamburgerActive = isActive;
     });
   }
 
+  /**
+  * Toggle die Navigation und aktualisiert den Routing-Pfad basierend auf dem Hamburger-Status.
+  */
   toggleNavigation() {
     this.sharedService.toggleHamburgerState();
-
     if (this.isHamburgerActive) {
       console.log('Hamburger is active');
       this.router.navigate(['navigation']);
@@ -27,5 +42,4 @@ export class HeaderComponent {
       this.router.navigate(['']);
     }
   }
-
 }
